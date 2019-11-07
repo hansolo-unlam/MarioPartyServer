3f7e825c8b20f761451911ae919317ac27e43769
LucasTeijo <54475564+LucasTeijo@users.noreply.github.com>
2019-11-07 18:08:56 -0300

    Desconectar cliente cuando cierra el lobby

--- src/paquetes/Paquete.java
+++ src/paquetes/Paquete.java
@@ -1,5 +1,6 @@
 package paquetes;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -13,65 +14,71 @@ public class Paquete {
 	protected String tipo;
 	protected int id;
 	protected int idPartida;
-	
+
 	private JsonObject jsonObject;
 	private String cabecera;
 	private JsonObject data;
 	private Socket socketCliente;
 
-	/*public Paquete(int id, String tipo, int idPartida) {
-		this.tipo = tipo;
-		this.id = id;
-		this.idPartida = idPartida;
-	}
+	/*
+	 * public Paquete(int id, String tipo, int idPartida) { this.tipo = tipo;
+	 * this.id = id; this.idPartida = idPartida; }
+	 * 
+	 * public Paquete(int id, String tipo) { this.id = id; this.tipo = tipo; }
+	 */
 
-	public Paquete(int id, String tipo) {
-		this.id = id;
-		this.tipo = tipo;
-	}*/
-	
 	public Paquete(JsonObject jsonObject, Socket socketCliente) {
 		this.jsonObject = jsonObject;
 		this.socketCliente = socketCliente;
 		verificarPaquete();
-		
+
 	}
-	
+
 	private void verificarPaquete() {
 		cabecera = jsonObject.get("nombre").getAsString();
 		data = jsonObject.get("data").getAsJsonObject();
 
-		switch(cabecera) {
-			case "LOGIN":
-					String usuario = data.get("usuario").getAsString();
-					String contraseña = data.get("contraseña").getAsString();
-					//verificarDatosUsuario(usuario, contraseña, socket);
-				break;
-				
-			case "INGRESAR_SALA":
-					String salaSolicitada = data.get("salaSolicitada").getAsString();
-					System.out.println("Paquete recibido");
-					Lobby.agregarASala(salaSolicitada, socketCliente);
-				break;
-				
-			case "INICIAR_PARTIDA":
-					//Sala.iniciarPartida(socket);
-				break;
-			
-			case "NUEVA_SALA":
-					String nombreSala = data.get("nombreSala").getAsString();
-					System.out.println("Paquete recibido");
-					//Lobby.crearSala(nombreSala, socketCliente);
-				break;
-				
-			case "SALIR_SALA":
-					String salaSolicitada1 = data.get("nombreSala").getAsString();
-					System.out.println("Paquete recibido");
-					//Lobby.sacarJugadorDeSala(salaSolicitada, socket)
-				break;
-				
-			default:
-				
+		switch (cabecera) {
+		case "SALIR":
+			try {
+				socketCliente.close();
+				System.out.println("Un cliente salio");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case "LOGIN":
+			String usuario = data.get("usuario").getAsString();
+			String contraseña = data.get("contraseña").getAsString();
+			// verificarDatosUsuario(usuario, contraseña, socket);
+			break;
+
+		case "INGRESAR_SALA":
+			String salaSolicitada = data.get("salaSolicitada").getAsString();
+			System.out.println("Paquete recibido");
+			Lobby.agregarASala(salaSolicitada, socketCliente);
+			break;
+
+		case "INICIAR_PARTIDA":
+			System.out.println("Paquete recibido");
+			// Sala.iniciarPartida(socket);
+			break;
+
+		case "NUEVA_SALA":
+			String nombreSala = data.get("nombreSala").getAsString();
+			System.out.println("Paquete recibido");
+			// Lobby.crearSala(nombreSala, socketCliente);
+			break;
+
+		case "SALIR_SALA":
+			String salaSolicitada1 = data.get("nombreSala").getAsString();
+			System.out.println("Paquete recibido");
+			// Lobby.sacarJugadorDeSala(salaSolicitada, socket)
+			break;
+
+		default:
+
 		}
 	}
 }
